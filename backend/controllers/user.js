23ed5c185eebe0765e32a675cad6bee5d23b545d
Artcann <art.cann@orange.fr>
2021-03-20 21:24:23 +0100

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,54 +4,47 @@ const nodemailer = require('nodemailer');
 
 const User = require('../models/User');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
+    try {
+        hash = await bcrypt.hash(req.body.password, 10)
 
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
+        const newUser = new User({
             pseudo: req.body.pseudo,
             email: req.body.email,
             password: hash,
             verif: req.body.verif
         });
-        user.save(function(err, user){
-            const userUid = user._id;
-        })
-        .then(
-            () => {
-                res.status(201).json({message: 'Utilisateur créé !'});
+        let user = await newUser.save()
+        res.status(201).json({message: 'Utilisateur créé !'});
 
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "login",
+                user: "overlap.noreply@gmail.com",
+                pass: "J?KgyqkjfL4}`6s3"
             }
-        ).catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+        });
 
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            type: "login",
-            user: "overlap.noreply@gmail.com",
-            pass: "J?KgyqkjfL4}`6s3"
-        }
-    });
+        const mailOptions = {
+            from: process.env.GMAIL_MAIL,
+            to: req.body.email,
+            subject: "Testing email Verification",
+            html: "<h1>Valide ton profil en cliquant sur ce lien !</h1><br><a href=http://localhost:3000/api/auth/verif/" + user._id +
+            ">Vérifier mon profil</a>"
+        };
 
-    const mailOptions = {
-        from: process.env.GMAIL_MAIL,
-        to: req.body.email,
-        subject: "Testing email Verification",
-        html: "<h1>Valide ton profil en cliquant sur ce lien !</h1><br><a href=https://localhost:3000/api/auth/verif/" +
-        ">Vérifier mon profil</a>"
-    };
-
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-
-    
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+   
 };
 
 exports.login = (req, res, next) => {
