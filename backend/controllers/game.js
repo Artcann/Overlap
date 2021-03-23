30b3dcc763d80622b6acdcbd0bdf4181eea19766
Artcann <art.cann@orange.fr>
2021-03-23 20:25:23 +0100

    Add Day Controller

--- backend/controllers/game.js
+++ backend/controllers/game.js
@@ -12,11 +12,28 @@ exports.getRanking = async (req, res, next) => {
     }
 };
 
-exports.getDayInfo = async (req, res, next) => {
+exports.getDay = async (req, res, next) => {
     try {
         let infos = await Day.findOne({ number: req.params.number });
 
         res.status(200).json(infos);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+exports.insertDay = async (req, res, next) => {
+    try {
+        const newDay = new Day({
+            number: req.body.number,
+            starting_date: new Date("2016-05-18T16:00:00Z"),
+            questions: req.body.questions,
+            background: req.body.background,
+            overlay: req.body.overlay,
+            intro_text: req.body.intro_text
+        })
+        let day = await newDay.save();
+        res.status(201).json({message: 'Jour créé !', day: day});
     } catch (err) {
         res.status(500).send(err);
     }
--- backend/routes/game.js
+++ backend/routes/game.js
@@ -5,5 +5,7 @@ const userCtrl = require('../controllers/user');
 const gameCtrl = require('../controllers/game');
 
 router.get('/ranking', gameCtrl.getRanking);
+router.get('/day/:number', gameCtrl.getDay);
+router.post('/day', gameCtrl.insertDay);
 
 module.exports = router;
