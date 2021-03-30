872119a0fff9bcbcde51e5a8c737ca1ec6782c3f
Jules Ponsaillé <jules.ponsaille@isep.fr>
2021-03-30 15:54:06 +0200

    add stle game route

--- frontend/src/routes/game/index.js
+++ frontend/src/routes/game/index.js
@@ -2,8 +2,21 @@ import { h } from 'preact';
 import style from './style.css';
 
 const Game = () => (
-  <div>
-    game
+  <div class={style.game}>
+    <div class={style.gameContainer}>
+      <div class={style.menu}>
+        <div class={style.profile}>
+          <img alt="Picture of your character" src="/assets/profile_pics/square/jeremy.jpg" />
+          {/* TODO: Add the overlay */}
+        </div>
+        <div class={style.menuText}>
+          <span class={style.point}>1738 points</span>
+          <a href="#">Menu</a>
+        </div>
+      </div>
+      <img class={[style.joycon, style.joyconLeft].join(' ')} src="/assets/joycon-left.svg" alt="Nintendo Switch's Joycon" />
+      <img class={[style.joycon, style.joyconRight].join(' ')} src="/assets/joycon-right.svg" alt="Nintendo Switch's Joycon" />
+    </div>
   </div>
 );
 
