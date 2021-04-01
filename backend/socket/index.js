ef4cd31a3e3f9a2079780c3aee3cc514246471f8
Jules Ponsaillé <jules.ponsaille@isep.fr>
2021-04-01 22:03:08 +0200

    add get question from socket
    Co-authored-by: Artcann <Artcann@users.noreply.github.com>

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -4,6 +4,10 @@ const User = require("../models/User");
 const Day = require("../models/Day");
 const Question = require("../models/Question");
 
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
 exports.socket = (io) => {
   io.use(socketioJwt.authorize({
     secret: process.env.RANDOM_TOKEN_SECRET,
@@ -20,18 +24,47 @@ exports.socket = (io) => {
         return
       }
 
-      const user = User.findOne({_id : socket.decoded_token.userId});
+      const user = await User.findOne({_id : socket.decoded_token.userId});
       const progression = user.progression || {};
 
-      const questions = await Promise.all(day.questions.map(async questionId => {
-        return await Question.findOne({id: questionId});
-      }))
+      // Add questions to the user
+      if (!progression[day._id]) {
+        const questions = await Promise.all(day.questions.map(async questionId => {
+          return await Question.findOne({id: questionId});
+        }))
+  
+        progression[day._id] = shuffle(questions).map(question => {
+          question.answer = null
+          question.time = null
+          return question
+        });
+        
+        try {
+          await User.findOneAndUpdate(
+            { 
+              _id: socket.decoded_token.userId 
+            },
+            {
+              progression: progression
+            }
+          );
+          } catch (e) {
+            socket.emit('error');
+          }
+      }
 
-      console.log(questions);
+      socket.emit('startGameSuccess')
+    })
 
-      console.log(progression)
-      if(!progression[day._id]) {
+    socket.on('nextQuestion', async () => {
+      const day = await Day.getDay();
+      const questions = (await User.findOne({_id: socket.decoded_token.userId})).progression[day._id];
 
+      for (const question of questions) {
+        if (!question.answer) {
+          socket.emit("nextQuestion", question);
+          return;
+        }
       }
     })
 
--- frontend/src/routes/game/quiz.js
+++ frontend/src/routes/game/quiz.js
@@ -4,14 +4,18 @@ import socketIOClient from 'socket.io-client';
 import style from'./style.css';
 
 import { AuthContext } from '../../contexts/auth'
+import { LanguageContext } from '../../translations';
+import { socket } from '../../../../backend/socket';
 
 const Quiz = () => {
   const { user } = useContext(AuthContext);
+  const { language } = useContext(LanguageContext);
 
   const socketRef = useRef();
   
   const [connected, setConnected] = useState(false);
   const [question, setQuestion] = useState(null);
+  const [error, setError] = useState(false);
 
 
   useEffect(() => {
@@ -20,33 +24,47 @@ const Quiz = () => {
       reconnection: true
     })
 
-    socketRef.current.once('connect', function () {
+    socketRef.current.once('connect', () => {
       if (!connected) {
         setConnected(true)
-        console.log('hey')
         socketRef.current.emit('startGame')
       }
     });
+
+    socketRef.current.on('error', () => {
+      setError(true)
+    })
+
+    socketRef.current.on('startGameSuccess', () => {
+      socketRef.current.emit('nextQuestion')
+    })
+
+    socketRef.current.on('nextQuestion', question => {
+      setQuestion(question)
+    })
   })
 
+  if (error)
+    return <h1>An error has occured. Please refresh.</h1>
 
   if (!connected)
     return <h1>Connecting...</h1>
 
-  return (
-    <>
-      <div class={style.gameQuestion}>
-        <h1>Question</h1>
-        <img class={style.gameScreen} src="https://1uyxqn3lzdsa2ytyzj1asxmmmpt-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/Lot-189-Keith-Haring-Retrospect-1-768x426.jpg" />
-      </div>
-      <div class={style.gameAnswers}>
-        <button class={[style.gameButton, style.valid].join(' ')}>Keith Haring</button>
-        <button class={style.gameButton}>Keith Haring</button>
-        <button class={[style.gameButton, style.invalid].join(' ')}>Keith Haring</button>
-        <button class={style.gameButton}>Keith Haring</button>
-      </div>
-    </>
-  )
+  if (question) {
+    return (
+      <>
+        <div class={style.gameQuestion}>
+          <h1>{question.question[language]}</h1>
+          <img class={style.gameScreen} src="https://1uyxqn3lzdsa2ytyzj1asxmmmpt-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/Lot-189-Keith-Haring-Retrospect-1-768x426.jpg" />
+        </div>
+        <div class={style.gameAnswers}>
+          {question.answers[language].map((answer, index) => (
+            <button class={[style.gameButton].join(' ')} key={index}>{answer}</button>
+          ))}
+        </div>
+      </>
+    )
+  }
 };
 
 export default Quiz;
