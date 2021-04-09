18eb16ff8423a61c4c6b679ca9b79f9a3dfa50e7
Jules Ponsaillé <jules.ponsaille@isep.fr>
2021-04-09 14:34:55 +0200

    test new reveal card

--- frontend/src/routes/register/index.js
+++ frontend/src/routes/register/index.js
@@ -10,13 +10,15 @@ import { LanguageContext } from '../../translations';
 const Register = () => {
   const { language, translations } = useContext(LanguageContext);
 
-  const [character, setCharacter] = useState(null);
-  /*let setCharacter = undefined
+  //const [character, setCharacter] = useState(null);
+  let setCharacter = undefined
   let character = {
     identifier: 'marjorie',
-    description: 'yolo',
+    description: {
+      fr: 'Ses convictions lui ont fait tirer un trait sur le saucisson mais heureusement pour elle, il n’y a pas de viande dans la bière. 😎 Vous l’aurez compris, Marjorie aime les soirées (surtout si elles sont arrosées). Partie à l’autre bout du globe pour découvrir de nouvelles traditions (ne vous méprenez pas, on ne parle pas de musées 🥳), elle reste néanmoins fidèle à sa liste même si cela implique des réunions à 5h du matin. 😳'
+    },
     name: 'Marjorie'
-  }*/
+  }
 
   return (
     <Match path="/start/reveal">
@@ -38,10 +40,11 @@ const Register = () => {
               <div class={[style.register, style.reveal].join(' ')}>
                 <div class={[style.container, style.reveal].join(' ')}>
                   <div class={[style.content, style.reveal].join(' ')}>
-                    <img class={style.revealImage} alt={character.name} src={`/assets/profile_pics/vertical/${character.identifier}.jpg`} />
+                    <img class={style.revealImage} alt={character.name} src={`/assets/profile_pics/square/${character.identifier}.jpg`} />
                     <h1>{character.name}</h1>
-                    <img class={style.ready} alt='Ready to fight' src='/assets/ready-to-fight.png' />
+                    <div class={style.square}></div>
                     <div class={style.revealText}>
+                      <img class={style.ready} alt='Ready to fight' src='/assets/ready-to-fight.png' />
                       <p>{character.description[language]}</p>
                       <h2>{translations.userInfos.emailCheckMessage}</h2>
                     </div>
