0dea9d11db4a1e3f4e0451015d2e61575a36d3d8
Jules Ponsaillé <jules.ponsaille@isep.fr>
2021-04-10 19:00:25 +0200

    link to form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,23 @@ import './style';
 import App from './components/app';
 import * as buffer from 'buffer';
 
+console.log(`
+ ___ _ __ _ __ ___  _ __ 
+/ _ \\ '__| '__/ _ \\| '__|
+| __/ |  | | | (_) | |   
+\\___|_|  |_|  \\___/|_|   \n
+/!\\ Error /!\\ Error /!\\ Error /!\\ \n
+/!\\  User has found a breach  /!\\ \n
+/!\\ Error /!\\ Error /!\\ Error /!\\ \n
+/!\\    Please remain calm !   /!\\ \n
+/!\\ Error /!\\ Error /!\\ Error /!\\ \n
+/!\\ Safety procedure initiated/!\\ \n
+/!\\Our agent is backing up all/!\\ \n
+/!\\      the pop culture      /!\\ \n
+/!\\  Code: odknpqnh_wp_mYEFT  /!\\ \n
+/!\\ Error /!\\ Error /!\\ Error /!\\ \n
+`)
+
 /* Polyfills */
 if (typeof window !== "undefined") {
   window.Buffer = buffer;
