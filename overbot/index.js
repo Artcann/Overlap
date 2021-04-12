d2f7938e915ae7e69d1c6ad9634fac6dbb6d3964
Artcann <art.cann@orange.fr>
2021-04-12 17:02:24 +0200

    Finalize Ranking Overbot

--- overbot/index.js
+++ overbot/index.js
@@ -1,62 +1,23 @@
 const Discord = require('discord.js');
-const jwt = require('jsonwebtoken');
 const path = require('path');
-const fetch = require('node-fetch');
+const cron = require('node-cron');
 const bot = new Discord.Client();
 require('dotenv').config({path: path.join(__dirname, '.env')});
 
-function fetchRanking() {
-    return fetch('http://localhost:3000/game/ranking', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwt.sign(
-            { userId: 'overBot' },
-            process.env.JWT_TOKEN
-            )}`
-        }
+const ranking = require('./utils/ranking');
+
+bot.once('ready', async () => {
+
+    const rawRanking = await ranking.fetchRanking();
+    const prettyRanking = ranking.prettifyRanking(rawRanking);
+
+    cron.schedule('* 15 * * *', () => {
+        bot.channels.fetch("831162103162994778")
+        .then(channel => channel.messages.fetch("831175547047641158")
+            .then(message => message.edit(prettyRanking))
+            .catch(err => console.log(err)))
+        .catch(console.error);
     })
-    .then( res => {
-        return res.json();
-    })
-    .then( data => {
-        return data;  
-    })
-    .catch(err => console.log( err ));
-}
-
-function prettifyRanking(ranking) {
-    let string = "";
-    for(let i=0; i < 25; i++) {
-        if(i === 0) {
-            string += `🥇 | ${ranking[i]["pseudo"]} (${ranking[i]["score"]} points)\n`;
-        } else if(i === 1) {
-            string += `🥈 | ${ranking[i]["pseudo"]} (${ranking[i]["score"]} points)\n`;
-        } else if(i === 2) {
-            string += `🥉 | ${ranking[i]["pseudo"]} (${ranking[i]["score"]} points)\n`;
-        } else {
-            string += `#${i+1} | ${ranking[i]["pseudo"]} (${ranking[i]["score"]} points)\n`;
-        }
-        
-    }
-    return string;
-}
-
-bot.on('ready', async () => {
-    console.log("Je suis connecté !");
-
-    bot.channels.fetch("830805431463051285")
-    .then(channel => channel.messages.fetch("830811600176545822")
-        .then(message => message.edit("Test 4"))
-        .catch(err => console.log(err)))
-    .catch(console.error);
-
-    const rawRanking = await fetchRanking();
-    const prettyRanking = prettifyRanking(rawRanking);
-
-    bot.channels.fetch("830805431463051285")
-    .then(channel => channel.send(prettyRanking))
-    .catch(err => console.log(err));
 });
 
 bot.login(process.env.DISCORD_TOKEN);
